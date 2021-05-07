083da555d71f62d41f6e4f973a9c767de21b3cb2
Tina Nguyen <nguyentinaca@yahoo.com>
2021-05-06 18:01:21 -0700

    new repo

---
+++ public/javascript/add-post.js
@@ -0,0 +1,25 @@
+async function newFormHandler(event) {
+  event.preventDefault();
+
+  const title = document.querySelector('textarea[name="title"]').value.trim();
+  const content = document.querySelector('textarea[name="content"]').value.trim();
+
+  const response = await fetch(`/api/posts`, {
+    method: 'POST',
+    body: JSON.stringify({
+      title,
+      content
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard/');
+  } else {
+    alert(response.statusText);
+  }
+};
+
+document.querySelector('#new-post-form')?document.querySelector('#new-post-form').addEventListener('submit', newFormHandler):null;
---
+++ utils/auth.js
@@ -0,0 +1,9 @@
+const withAuth = (req,res, next) => {
+    if (!req.session.loggedIn) {
+        res.redirect('/login');
+        return;
+      } 
+      next ();
+};
+
+module.exports = withAuth;
